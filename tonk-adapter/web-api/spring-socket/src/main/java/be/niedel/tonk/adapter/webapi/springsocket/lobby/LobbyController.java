e420488d5f9d535bd3a387c31922930914de4035
nielsdelestinne <nielsdelestinne@gmail.com>
2019-08-12 19:46:30 +0200

    Injecting sendMessageLobbyUseCase in LobbyController and using its process method + created Bean configuration for Lobby use cases

--- tonk-adapter/web-api/spring-socket/src/main/java/be/niedel/tonk/adapter/webapi/springsocket/lobby/LobbyController.java
+++ tonk-adapter/web-api/spring-socket/src/main/java/be/niedel/tonk/adapter/webapi/springsocket/lobby/LobbyController.java
@@ -1,6 +1,7 @@
 package be.niedel.tonk.adapter.webapi.springsocket.lobby;
 
 import be.niedel.tonk.application.lobby.sendmessage.LobbyMessageDto;
+import be.niedel.tonk.application.lobby.sendmessage.SendMessageLobbyUseCase;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -9,10 +10,16 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class LobbyController {
 
+    private final SendMessageLobbyUseCase sendMessageLobbyUseCase;
+
+    public LobbyController(SendMessageLobbyUseCase sendMessageLobbyUseCase) {
+        this.sendMessageLobbyUseCase = sendMessageLobbyUseCase;
+    }
+
     @MessageMapping("/send-lobby-message")
     @SendTo("/topic/lobby-messages")
     public LobbyMessageDto sendMesssage(@Payload LobbyMessageDto message) {
-        return message;
+        return sendMessageLobbyUseCase.process(message);
     }
 
 }
